5a6d510759763dca7117aabebadb88adc86f24ba
Marc Köhlbrugge <subscriptions@marckohlbrugge.com>
2018-03-29 18:26:27 +0700

    remove user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
-
   def show
     @user = User.find_by! profile_token: params[:id]
     @meals = @user.meals
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   telegram_webhook TelegramWebhooksController
 
   resources :users, only: [:show]
-  # get "@:id", to: "users#show", as: :user
 
   root to: "pages#home"
 end
