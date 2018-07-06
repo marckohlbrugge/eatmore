d16a997251c8f992fd3b7ef514507798915178b6
Marc Köhlbrugge <subscriptions@marckohlbrugge.com>
2018-07-06 21:53:32 +0200

    set default url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  routes.default_url_options = { host: ENV.fetch("HOST"), protocol: "https" }
 end
